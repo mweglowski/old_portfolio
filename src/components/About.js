2ea4df33d01d2515fd80a0a1642b8bd76c7b8b32
Marcin Weglowski <marcin.weglowski1@gmail.com>
2022-11-01 22:04:03 +0100

    some work some problems some satisfactions

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import "./About.css";
+
+const About = (props) => {
+  return (
+    <section className="about">
+      <div className="section-title">About</div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import portrait from "../images/portrait.jpg";
+import "./Home.css";
+
+const Home = (props) => {
+  return (
+    <div className="home">
+      <div className="title">
+        <div className="title-welcome"></div>
+        <div className="title-name"></div>
+        <div className="title-profession"></div>
+        <button className='home-button'>Contact</button>
+      </div>
+      {/* <img className="portrait-image" src={portrait} alt="portrait-image" /> */}
+    </div>
+  );
+};
+
+export default Home;
