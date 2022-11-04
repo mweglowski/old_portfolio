c8ba0e904de9b4c5143c05098f6ba0b45c4d81cd
Marcin Weglowski <marcin.weglowski1@gmail.com>
2022-11-04 18:18:44 +0100

    skills section animations finished

--- src/components/Skills/Skill.js
+++ src/components/Skills/Skill.js
@@ -1,12 +1,24 @@
+import { useInView } from "react-intersection-observer";
 import "./Skill.css";
 
 const Skill = (props) => {
+  const { ref: skillLiquidRef, inView: isSkillLiquidRefVisible } = useInView();
+  const { skillData } = props;
+
   return (
     <div className="skill">
-      <div className="skill-name">ReactJS</div>
+      <div className="skill-name">{skillData.name}</div>
       <div className="skill-chart-box">
         <div className="skill-boiler">
-          <div className="skill-liquid">77%</div>
+          <div
+            className={`skill-liquid ${
+              isSkillLiquidRefVisible && "fill-boiler"
+            }`}
+            style={{width: `${skillData.level}%`}}
+            ref={skillLiquidRef}
+          >
+            {`${skillData.level}%`}
+          </div>
         </div>
       </div>
     </div>
--- src/components/Skills/SkillsList.js
+++ src/components/Skills/SkillsList.js
@@ -1,10 +1,23 @@
-import Skill from "./Skill"
-import "./SkillsList.css"
+import Skill from "./Skill";
+import "./SkillsList.css";
 
 const SkillsList = () => {
-	return <div className="skills-list">
-		<Skill />
-	</div>
-}
+  const skills = [
+    { name: "ReactJS", level: 77 },
+    { name: "Redux", level: 45 },
+    { name: "SQL", level: 68},
+    { name: "Python", level: 65 },
+    { name: "CSS", level: 88 },
+    { name: "HTML", level: 85 },
+  ];
+
+  return (
+    <div className="skills-list">
+      {skills.map((skillItem) => (
+        <Skill skillData={skillItem} key={skillItem.name} />
+      ))}
+    </div>
+  );
+};
 
 export default SkillsList;
