2e019f60e9ded06b557b73780300d98b6df1d22e
Marcin Weglowski <marcin.weglowski1@gmail.com>
2022-11-10 17:15:45 +0100

    project technologies

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,3 +1,8 @@
+import React from "react";
+import { ReactComponent as JavaScriptIcon } from "../../images/js.svg";
+import { ReactComponent as ReactIcon } from "../../images/js.svg";
+import CSSIcon from "../../images/css.png";
+import { ReactComponent as HTMLIcon } from "../../images/js.svg";
 import classes from "./ProjectCard.module.css";
 
 const ProjectCard = (props) => {
@@ -14,6 +19,61 @@ const ProjectCard = (props) => {
           alt={projectData.description}
         />
         <div className={classes.buttonBlur} />
+      </div>
+      <div className={classes.technologies}>
+        {projectData.technologies.map((tech) => {
+          if (tech === "JavaScript") {
+            return (
+              <span
+                className={classes.tech}
+                style={{ color: "yellow", width: "fit-content" }}
+                key={tech}
+              >
+                JS
+              </span>
+            );
+          } else if (tech === "CSS") {
+            return (
+              <span
+                className={classes.tech}
+                style={{ color: "#0099ff", width: "fit-content" }}
+                key={tech}
+              >
+                CSS
+              </span>
+            );
+          } else if (tech === "SCSS") {
+            return (
+              <span
+                className={classes.tech}
+                style={{ color: "#f9627b", width: "fit-content" }}
+                key={tech}
+              >
+                SCSS
+              </span>
+            );
+          } else if (tech === "HTML") {
+            return (
+              <span
+                className={classes.tech}
+                style={{ color: "#e04327", width: "fit-content" }}
+                key={tech}
+              >
+                HTML
+              </span>
+            );
+          } else if (tech === "React") {
+            return (
+              <div
+                className={classes.tech}
+                style={{ color: "cyan", width: "fit-content" }}
+                key={tech}
+              >
+                REACT
+              </div>
+            );
+          } else return;
+        })}
       </div>
     </div>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,9 +12,10 @@ const Projects = React.forwardRef((props, ref) => {
   const projects = [
     {
       id: 0,
-      title: "Tribute Page",
-      description: "Page dedicated to the memory of Steve Jobs",
-      image: tributePageImage,
+      title: "Boat Booking",
+      description: "App that enables us to book some boats in a simple way",
+      image: boatBookingPageImage,
+      technologies: ["React", "HTML", "CSS"],
     },
     {
       id: 1,
@@ -22,30 +23,35 @@ const Projects = React.forwardRef((props, ref) => {
       description:
         "Page of ficitive company that specialises in selling medicine - Shopping Cart",
       image: medreactPageImage,
+      technologies: ["React", "HTML", "CSS"],
     },
     {
       id: 2,
-      title: "Survey Form",
-      description: "Beautiful design of form",
-      image: surveyFormPageImage,
+      title: "Tribute Page",
+      description: "Page dedicated to the memory of Steve Jobs",
+      image: tributePageImage,
+      technologies: ["HTML", "CSS"],
     },
     {
       id: 3,
-      title: "Escape The Virus",
-      description: "Game made in plain JS where you have to escape the virus",
-      image: escapeTheVirusPageImage,
+      title: "Survey Form",
+      description: "Beautiful design of form",
+      image: surveyFormPageImage,
+      technologies: ["HTML", "CSS"],
     },
     {
       id: 4,
-      title: "Hangman",
-      description: "You have to guess a hidden word",
-      image: hangmanPageImage,
+      title: "Escape The Virus",
+      description: "Game made in plain JS where you have to escape the virus",
+      image: escapeTheVirusPageImage,
+      technologies: ["JavaScript", "HTML", "CSS"],
     },
     {
       id: 5,
-      title: "Boat Booking",
-      description: "App that enables us to book some boats in a simple way",
-      image: boatBookingPageImage,
+      title: "Hangman",
+      description: "You have to guess a hidden word",
+      image: hangmanPageImage,
+      technologies: ["JavaScript", "HTML", "SCSS"],
     },
   ];
 
